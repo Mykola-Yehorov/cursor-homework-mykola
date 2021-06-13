c0fdc2c75fe283a187dd8c8aff86ac1604748a39
Nick <pr2.nikolas@gmail.com>
2021-06-13 12:46:12 +0300

    Homework-3 13-06

--- hw3/main.js
+++ hw3/main.js
@@ -1,38 +1,47 @@
 
 //1. Створити функцію getMaxDigit(number) – яка отримує будь-яке число та виводить найбільшу цифру в цьому числі.
-/*
 function getMaxDigit(number){    
-  stringFromNumbers = Array.from(String(number));
-  Array.from(String(number)).map(el =>)   
-  } 
-  const maxNumber = Math.max(arrayOfNumbers); 
+  const stringFromNumbers = Array.from(String(number));
+  for (let i = 0; i < stringFromNumbers.length; i++){
+    stringFromNumbers[i] = Number (stringFromNumbers[i]);    
+  }     
+  const maxNumber = Math.max(...stringFromNumbers); 
   return maxNumber;
 }
-console.log (getMaxDigit(987));
+console.log (`Максимальна цифра в числі 5830 становить ${getMaxDigit(5830)}`);
 
+//2. Створити функцію, яка визначає ступінь числа
+function powerNumber(number, power){
+  let powerNumberAnswer = 1;
+  for (let i=1; i<=power; i++){
+    if (power >= 0)
+      powerNumberAnswer = powerNumberAnswer*number;     
+    else
+      powerNumberAnswer = powerNumberAnswer*(1/number);     
+  }
+  return powerNumberAnswer;
+} 
+console.log (`Число 5 в 2 ступені дорівнює ${powerNumber(5, 2)}`);
 
-*/
 //3. Створити функцію, яка форматує ім'я, роблячи першу букву великою.
-/*
 function FirstLetterCapital (text){
   const lowerСase = text.toLowerCase();
   return lowerСase.charAt(0).toUpperCase() + lowerСase.slice(1);
 }
-console.log (FirstLetterCapital("мАрИнА"));
+console.log (`Відформатували слово "мАрИнА" - ${FirstLetterCapital("мАрИнА")}`);
 
 //4. Створити функцію, яка вираховує суму, що залишається після оплати податку від зарабітньої плати.
-const netSalary = (preTaxSalary, tax) => preTaxSalary*(1-tax/100)
-console.log (netSalary(1000, 19.5));
+const netSalary = (preTaxSalary, tax) => Math.round(preTaxSalary*(1-tax/100))
+console.log (`Сума, що залишається після оплати податку (19,5%) від зарабітньої плати (1000 грн)  - ${netSalary(1000, 19.5)} грн.`);
 
 //5. Створити функцію, яка повертає випадкове ціле число в діапазоні від N до M
 function getRandomNumber(firstNumber, secondNumber){
   const randomNumber = (Math.random() * (secondNumber - firstNumber + 1) + firstNumber);
   return Math.floor (randomNumber);
 }
-console.log (getRandomNumber (1,3));
+console.log (`Випадкове ціле число в діапазоні від 1 до 5: ${getRandomNumber (1,5)}`);
 
 //6. Створити функцію, яка рахує скільки разів певна буква повторюється в слові
-
 function countLetter(searchLetter, word){
   const arrFromWord = Array.from(String(word));
   let letterCounter = 0;
@@ -42,8 +51,8 @@ function countLetter(searchLetter, word){
   }
   return letterCounter;
 }
-console.log (countLetter ('а','мама'));
-*/
+console.log (`${countLetter ('а','мама')} рази (разiв) буква a повторюється в слові мама`);
+
 //7. Створіть функцію, яка конвертує долари в гривні та навпаки в залежності від наявності символа $ або UAH в рядку
 function convertCurrency(money){
   const exchangeRate = 27.1;
@@ -60,7 +69,6 @@ function convertCurrency(money){
     amountMoney = Number(amountMoney);
   }
   else console.error("Помилка! Зазначена валюта не конвертується даною функцією");
-
   let convertResultRoundes = 0;
   if (UpperCaseMoney.includes("$")){
     const convertResult =  exchangeRate * amountMoney;
@@ -73,11 +81,9 @@ function convertCurrency(money){
   }
   return convertResultRoundes;
 }
-console.log (convertCurrency ('100$'));
+console.log (`За 100 доларів Ви отримаєте ${convertCurrency ('100$')} грн.`);
 
 //8. Створіть функцію генерації випадкового паролю (тільки числа), довжина встановлюється користувачем або по замовчуванню = 8 символам.
-//getRandomPassword(4)
-
 function getRandomPassword(len = 8){
   let password = '';
   for (i=0;i<len;i++){
@@ -85,7 +91,7 @@ function getRandomPassword(len = 8){
   }  
   return password ;
 }
-console.log(getRandomPassword(4));
+console.log (`Сгенерували випадковий пароль (тільки числа), довжина якого становить 4 символи: ${getRandomPassword(4)}`);
 
 //9. Створіть функцію, яка видаляє всі букви з речення
 function deleteLetters(letterToDelete, word){
@@ -99,22 +105,30 @@ function deleteLetters(letterToDelete, word){
   return (WordToChange);
 }
 console.log (deleteLetters("о","молоко"));
+console.log (`Видалили всі букви 0 з слова "молоко": ${getRandomPassword(4)}`);
 
 //10. Створіть функцію, яка перевіряє, чи є слово паліндромом
 function isPalyndrom(word){
   const reverseWord = word.toLowerCase().split('').reverse().join('');
   //const isPalyndromAnswer = "";
   if (word.toLowerCase() == reverseWord)
-    return (`Слово ${word} є паліндромом`)
+    return (`${word} є паліндромом`)
   else
-    return (`Слово ${word} не є паліндромом`) 
+    return (`${word} не є паліндромом`) 
 }
-console.log (isPalyndrom("Дід"));
+console.log (`Слово ${isPalyndrom("Дід")}`);
 
 
-
-/*
-const isPalyndrom = (word) => preTaxSalary*(1-tax/100)
-console.log ("дід");
-*/
+//11. Створіть функцію, яка видалить з речення букви, які зустрічаються більше 1 разу 
+function deleteDuplicateLetter(string){
+  let stringWithountDuplicateLetter = '';
+  for (let i = 0; i < string.length; i++) {
+      const regExp = new RegExp(string[i], 'gi');
+      if (string.match(regExp).length <= 1){
+        stringWithountDuplicateLetter = stringWithountDuplicateLetter + string[i];   
+      }             
+  }
+  return stringWithountDuplicateLetter;
+}
+console.log (`Видалили з речення "Бісквіт був дуже ніжним" букви, які зустрічаються більше 1 разу: ${deleteDuplicateLetter("Бісквіт був дуже ніжним")}`);
 
