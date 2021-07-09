0b834ed054c3bd2e57a172b4dd482c7edf38c3ae
Nick <pr2.nikolas@gmail.com>
2021-07-09 17:05:11 +0300

    Homework-10 09-07 17:05

--- hw10/main.js
+++ hw10/main.js
@@ -1,50 +1,20 @@
-/*function helloTeam(){
-  console.log('Hello Team')
+function gammaPlay(event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const button = document.querySelector(`.button[data-key="${event.keyCode}"]`);
+  audio.play();
+  button.classList.add('active');
 }
-*/
-//document.querySelector('#myClick').onclick = helloTeam;
-//document.querySelector('#myClick').addEventListener('click', helloTeam);
 
-/*
-const itemOne = document.querySelector('.item-one');
-const itemTwo = document.querySelector('.item-two');
-const itemThree = document.querySelector('.item-three');
+const buttons = document.querySelectorAll('.button');
 
-itemOne.addEventListener('click', function(e) {
-  console.log (111);
-  e.stopPropagation();
+buttons.forEach((button) => {
+  button.onclick = function () {
+    const audio = document.querySelector(
+      `audio[data-key="${button.dataset.key}"]`
+    );
+    audio.play();
+    button.classList.add('active');
+  };
 });
 
-itemTwo.addEventListener('click', function(e) {
-  console.log (222);
-  e.stopPropagation();
-});
-
-itemThree.addEventListener('click', function(e) {
-  console.log (333);
-  e.stopPropagation();
-});
-
-
-const list = document.querySelector('ul');
-
-list.addEventListener('click', e => {
-  if(e.traget.tagName === "LI"){
-    e.traget.innerHTML = e.traget.innerHTML ** 2
-  }
-})*/
-
-const list = document.querySelector('ul');
-const addLi = document.querySelector('#add-bumber');
-
-list.addEventListener('click', e => {
-  if (e.target.closest("li")){
-    e.target.inerrHTML = e.traget.innerHTML ** 2;
-  }
-})
-
-addLi.addEventListener('click', function(){
-  let li = document.createElement('li');
-  li.innerHTML = "<input>";
-  list.appendChild(li);
-})
+window.addEventListener('keydown', gammaPlay);
